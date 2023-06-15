bfa6eaca71bb217fc1d6034e6a638e55b99d2e08
CleeKru <cleekru@gmail.com>
2023-06-15 11:33:46 +0200

    shortest pair for Expansion and code cleanup

--- src/main/java/at/ac/tuwien/ifs/sge/agent/AbstractMacroAction.java
+++ src/main/java/at/ac/tuwien/ifs/sge/agent/AbstractMacroAction.java
@@ -2,10 +2,14 @@ package at.ac.tuwien.ifs.sge.agent;
 
 import at.ac.tuwien.ifs.sge.core.engine.logging.Logger;
 import at.ac.tuwien.ifs.sge.game.empire.core.Empire;
+import at.ac.tuwien.ifs.sge.game.empire.model.units.EmpireUnit;
+import java.util.List;
 
 public abstract class AbstractMacroAction<A> implements MacroAction<A>{
 
     protected GameStateNode<A> gameStateNode;
+
+    protected final List<EmpireUnit> units;
 
     protected Empire game;
 
@@ -18,6 +22,7 @@ public abstract class AbstractMacroAction<A> implements MacroAction<A>{
     public AbstractMacroAction(GameStateNode<A> gameStateNode, int playerId, Logger log, boolean simulation) {
         this.gameStateNode = gameStateNode;
         this.game = (Empire) gameStateNode.getGame();
+        this.units = game.getUnitsByPlayer(playerId);
         this.playerId = playerId;
         this.log = log;
         this.simulation = simulation;
