0a0cd0dd993b590427232aed391cdd8f262906c1
moritzsattl <moritz.sattlegger@gmail.com>
2024-02-28 17:00:27 +0100

    first commit

--- src/main/java/at/ac/tuwien/ifs/sge/agent/util/UnitRole/OrderType.java
+++ src/main/java/at/ac/tuwien/ifs/sge/agent/util/UnitRole/OrderType.java
@@ -1,7 +1,25 @@
 package at.ac.tuwien.ifs.sge.agent.util.UnitRole;
 
 public enum OrderType {
-    PRODUCE,
-    MOVE;
+    PRODUCE("PRODUCE"),
+    MOVE("MOVE"),
+    EXPANSION("EXPANSION"),
+    CONQUER("CONQUER"),
 
+    DO_NOTHING("DO_NOTHING"),
+
+    SCHEDULE_NOTHING("SCHEDULE_NOTHING"),
+
+    WAIT("WAIT");
+
+    private final String type;
+
+    OrderType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
 }
